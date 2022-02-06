ff9eb75ac1c80c7a36edc4b0587ff62d16405a57
rhoribe <rhoribe1908@gmail.com>
2022-02-06 10:01:21 -0300

    disable  backend terraform cloud

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "remote" {
-    organization = "rh-aws-lab"
-    workspaces {
-      name = "terraform-lab"
-    }
-  }
-}
+# terraform {
+#   backend "remote" {
+#     organization = "rh-aws-lab"
+#     workspaces {
+#       name = "terraform-lab"
+#     }
+#   }
+# }
 
