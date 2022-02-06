c9bf75b9d25f2edfa0b691fa41ef9f23e07ac059
rhoribe <rhoribe1908@gmail.com>
2022-02-06 09:45:52 -0300

    fix ec2 outputs

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,6 +1,3 @@
-output "instance" {
-  value = aws_instance.ec2.id
-}
 
 output "private_ip" {
   value = aws_instance.ec2.private_ip
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "instance" {
-  value = module.ec2.id
-}
-
 output "private_ip" {
   value = module.ec2.private_ip
 }
