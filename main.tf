430e00d9f5395c3ef9de5eb4885981f75c3353d0
rhoribe <rhoribe1908@gmail.com>
2022-02-06 11:15:58 -0300

    fix infra

--- main.tf
+++ main.tf
@@ -7,24 +7,24 @@ resource "random_id" "random_sufix" {
 }
 
 
-# module "vpc" {
-#   source            = "./modules/vpc"
-#   vpc_cidr          = local.vpc_config.vpc_cidr
-#   vpc_name          = local.vpc_config.vpc_name
-#   subnet_cidr       = local.vpc_config.subnet_cidr
-#   availability_zone = local.vpc_config.availability_zone
-#   subnet_name       = local.vpc_config.subnet_name
-# }
+module "vpc" {
+  source            = "./modules/vpc"
+  vpc_cidr          = local.vpc_config.vpc_cidr
+  vpc_name          = local.vpc_config.vpc_name
+  subnet_cidr       = local.vpc_config.subnet_cidr
+  availability_zone = local.vpc_config.availability_zone
+  subnet_name       = local.vpc_config.subnet_name
+}
 
-# module "ec2" {
-#   source        = "./modules/ec2"
-#   count         = local.ec2_config.count
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = local.ec2_config.instance_type
-#   instance_name = "${local.ec2_config.instance_name}-${count.index}"
-#   subnet_id     = module.vpc.subnet_id
-#   depends_on    = [module.vpc]
-# }
+module "ec2" {
+  source        = "./modules/ec2"
+  count         = local.ec2_config.count
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = local.ec2_config.instance_type
+  instance_name = "${local.ec2_config.instance_name}-${count.index}"
+  subnet_id     = module.vpc.subnet_id
+  depends_on    = [module.vpc]
+}
 
 module "s3" {
   source = "./modules/s3"
